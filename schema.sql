17df69915c0b3b80c2ee990da46c263dbbf15e89
Bernhard Wendel <BWendel@gmx.de>
2016-03-31 21:32:53 +0200

    MySQL-Schema

---
+++ schema.sql
@@ -0,0 +1,26 @@
+﻿--
+-- Datenbank: `facebook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` varchar(15) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `is_active` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
