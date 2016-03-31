147d114b5a73a73723ae317c108f143048e6887c
Bernhard Wendel <BWendel@gmx.de>
2016-03-31 18:04:59 +0200

    Sample Config File

---
+++ config.sample.php
@@ -0,0 +1,22 @@
+<?php
+
+<?php
+
+$config = array();
+
+// Facebook-Settings
+$config["Facebook"] = array();
+$config["Facebook"]["AppID"] = "{App-ID}";
+$config["Facebook"]["AppSecret"] = "{App-Secret}";
+
+// Database Settings
+$config["DB"] = array();
+$config["DB"]["Host"] = "localhost";
+$config["DB"]["User"] = "root";
+$config["DB"]["Password"] = "root";
+
+
+?>
+
+
+?>
